1f4da21a6ae170fc991c7de9cdf8d588a238dfd0
Oscar Tsang <ohhtsang@gmail.com>
2020-04-30 00:49:32 +0100

    add files

---
+++ server/createRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const createRouter = function(data){
+  const router = express.Router();
+
+  router.get('/', function(req, res){
+    res.json(data);
+  })
+  router.post('/', function(req, res){
+    data.push(req.body);
+    res.json(data);
+  })
+  return router
+};
+
+module.exports = createRouter;
